6fd5f8bf5b994f386f4faca5cdc574b97d029ad9
aiden30015 <137600745+aiden30015@users.noreply.github.com>
2024-06-14 13:35:28 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var figlet = require("figlet");
 
 figlet("Hello World!!", function (err, data) {
-  if (err) {
+  if (err) {//황지훈 1cm
     console.log("Something went wrong...");
     console.dir(err);
     return;
